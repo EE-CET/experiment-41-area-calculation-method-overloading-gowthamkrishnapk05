96628085ecc4ddf47aebc940427f622ecc269b86
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-02-06 09:38:50 +0530

    updated

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -8,6 +8,20 @@ public class AreaCalculator {
     // TODO: Define calculateArea(float length, float breadth) -> returns area of rectangle
 
     // TODO: Define calculateArea(double base, double height) -> returns area of triangle
+    double calculateArea(float radius)
+    {
+        return Math.PI*radius*radius;
+    }
+
+    double calculateArea(float length, float breadth)
+    {
+        return length*breadth;
+    }
+
+    double calculateArea(double base, double height)
+    {
+        return 0.5*base*height;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,5 +31,18 @@ public class AreaCalculator {
         
         // TODO: Call methods and print results formatted to 2 decimal places
         // Hint: Use System.out.printf("%.2f\n", area);
+        float r,l,b;
+        double base,height;
+        AreaCalculator a1 = new AreaCalculator();
+        r=sc.nextFloat();
+        l=sc.nextFloat();
+        b=sc.nextFloat();
+        base=sc.nextDouble();
+        height=sc.nextDouble();
+        System.out.printf("%.2f\n",a1.calculateArea(r));
+        System.out.printf("%.2f\n",a1.calculateArea(l,b));
+        System.out.printf("%.2f\n",a1.calculateArea(height,base));
+        
+
     }
 }
